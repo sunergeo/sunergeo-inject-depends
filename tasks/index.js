f3f30f5b43e1bbe554b7f878a58c69f948c2da40
Shawn Bradley <sbradley@sunergeosystems.com>
2017-10-06 16:11:32 -0500

    Updated to support file copy from bower dependencies, written in correct order to target file, based on output from wiredep

--- tasks/index.js
+++ tasks/index.js
@@ -1,13 +1,29 @@
 ﻿module.exports = function (grunt) {
     var path = require('path');
+    const fs = require('fs');
+    var bower_dependencies = require('wiredep')();
 
     grunt.registerMultiTask('sunergeo-inject-depends', 'Build output of dependency modules and generate list of includes.', function () {
-
         grunt.log.ok('Generating dependency list and updating ' + this.data.target + '.');
 
         var fileList = grunt.file.expand(this.data.js, this.data.js.src);
         var tpl = this.data.js.tpl;
+        var dest = this.data.js.dest;
         var js_inc = "";
+        bower_dependencies.js.forEach(function (file) {
+            var destfile = path.resolve(__filename, dest + "/" + path.basename(file));
+            destfile = path.normalize(path.resolve(dest) + "/" + path.basename(file));
+            grunt.log.ok("Copying file " + file + " to " + destfile);
+
+            const readableStream = fs.createReadStream(file);
+            var writableStream = fs.createWriteStream(destfile);
+            readableStream.pipe(writableStream);
+
+            var inc = tpl.replace("[[filename]]", path.basename(file));
+            js_inc += inc + "\r\n";
+
+            grunt.log.ok("Writing new include tag " + inc);
+        });
         fileList.forEach(function (file) {
             var inc = tpl.replace("[[filename]]", path.basename(file));
             js_inc += inc + "\r\n";
@@ -16,7 +32,22 @@
 
         fileList = grunt.file.expand(this.data.css, this.data.css.src);
         tpl = this.data.css.tpl;
+        dest = this.data.css.dest;
         var css_inc = "";
+        bower_dependencies.css.forEach(function (file) {
+            var destfile = path.resolve(__filename, dest + "/" + path.basename(file));
+            destfile = path.normalize(path.resolve(dest) + "/" + path.basename(file));
+            grunt.log.ok("Copying file " + file + " to " + destfile);
+
+            const readableStream = fs.createReadStream(file);
+            var writableStream = fs.createWriteStream(destfile);
+            readableStream.pipe(writableStream);
+
+            var inc = tpl.replace("[[filename]]", path.basename(file));
+            css_inc += inc + "\r\n";
+
+            grunt.log.ok("Writing new include tag " + inc);
+        });
         fileList.forEach(function (file) {
             var inc = tpl.replace("[[filename]]", path.basename(file));
             css_inc += inc + "\r\n";
